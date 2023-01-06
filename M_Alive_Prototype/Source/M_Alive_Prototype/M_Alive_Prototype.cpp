2a91b35be54df739b4e4b913c833c71329f083ee
asakayuuki <sosueyakiko@icloud.com>
2023-01-06 14:37:54 +0800

    Update

--- M_Alive_Prototype/Source/M_Alive_Prototype/M_Alive_Prototype.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "M_Alive_Prototype.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, M_Alive_Prototype, "M_Alive_Prototype" );
--- M_Alive_Prototype/Source/M_Alive_Prototype/M_Alive_PrototypeGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "M_Alive_PrototypeGameModeBase.h"
-
